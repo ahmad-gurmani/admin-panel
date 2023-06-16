31676da170edca97fbbb24599d57b665ce8024cf
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2023-06-16 20:11:13 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500">
+        Hello world!
+      </h1>
+      <div className="w-full flex flex-col gap-2">
+      </div>
+    </>
+  )
+}
+
+export default App
